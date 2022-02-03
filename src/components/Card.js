1e48847006249d5ae66831e9d082d862e4a230ca
deltavitor <cvitor1618@gmail.com>
2022-02-03 20:30:20 -0300

    Changed Card component so that it uses props instead of hard coded data

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,18 @@
 import React from "react"
 
 import Star from "../images/star.svg"
-import KatieZaferes from "../images/cards/katie-zaferes.png"
 
-function Card() {
+function Card(props) {
   return (
     <section className={'card'}>
-      <img className={'card--image'} src={KatieZaferes}/>
+      <img className={'card--image'} src={require('../images/cards/' + props.img)}/>
       <div className={'card--stats'}>
         <img className={'card--star'} src={Star}/>
-        <p>5.0</p>
-        <p className={'text--gray'}>(6) • USA</p>
+        <p>{props.rating}</p>
+        <p className={'text--gray'}>({props.reviewCount}) • {props.country}</p>
       </div>
-      <p>Life lessons with Katie Zaferes</p>
-      <p><strong>From $136</strong> / person</p>
+      <p>{props.title}</p>
+      <p><strong>From ${props.price}</strong> / person</p>
     </section>
   )
 }
